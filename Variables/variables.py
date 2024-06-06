80b262cfbccea66d0371ab237573527420d62f0a
Cobian04 <jancobian19@gmail.com>
2024-06-05 18:17:33 -0600

    Primer hora del video

---
+++ Variables/variables.py
@@ -0,0 +1,14 @@
+edad = 10
+edad +=1
+print(edad)
+
+##Concatenacion
+nombre = "Jan"
+apellido = "Cobian"
+nombreCompleto = nombre + " " + apellido
+print(nombreCompleto)
+
+##Format
+segundoApellido = "Garcia"
+nombreEntero = f"Jan Cobian {segundoApellido}"
+print(nombreEntero)
---
+++ tipos-de-datos/datos-simples.py
@@ -0,0 +1,26 @@
+##Strings
+
+"Cadena"
+
+'String'
+
+'''Esta forma de colocar comillas
+    permite escribir
+    en varios renglones'''
+"""Esta foram de colocar comillas
+    permite escribir
+    en varios renglones"""
+
+##Numeros
+
+##Tipo INT
+40
+
+##Tipo float
+40.1
+
+
+##Booleanos
+
+False
+True
